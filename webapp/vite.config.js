630b893fe5da7a485c36a946b622fc3fd4e949c0
Fady Youssef <62950351+JustFady@users.noreply.github.com>
2026-04-28 15:44:13 -0700

    Update base path to cpsc481_project

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/CPSC481_personalProject/',
+  base: '/cpsc481_project/',
   plugins: [react()],
 });
